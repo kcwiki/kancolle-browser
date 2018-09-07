a5d85f012f73f8fe0c2d5607279cbc00a87f5acd
gk <gkpyon@gmail.com>
2018-09-07 23:23:53 +0900

    fix: sort everything in api_start2 object

--- handle-api.js
+++ handle-api.js
@@ -1,29 +1,16 @@
 const { readFileSync, outputJsonSync } = require('fs-extra')
 const { spawnSync } = require('child_process')
+const sortKeys = require('sort-keys')
+
+const parseApi = s => sortKeys(JSON.parse(s.replace(/^.*svdata=/, '')).api_data, { deep: true })
+
+const sh = (command, ...args) => {
+  process.stdout.write(spawnSync(command, args).stdout.toString())
+}
 
 const apiResponse = readFileSync(`${__dirname}/dist/api_start2`).toString()
 
-const parseApi = s => {
-  try {
-    return JSON.parse(s.replace(/^.*svdata=/, '')).api_data
-  } catch (_) {
-    return null
-  }
-}
-
-const sortKeys = o => {
-  Object.keys(o)
-    .sort()
-    .forEach(k => {
-      const v = o[k]
-      delete o[k]
-      o[k] = v
-    })
-}
-
 const api = parseApi(apiResponse)
-
-sortKeys(api.api_mst_const)
 
 outputJsonSync(`${__dirname}/../kancolle-data/dist/api_start2.json`, api, { spaces: 2 })
 
@@ -31,8 +18,6 @@ console.log('saved api_start2.json')
 
 const ts = new Date().toISOString().split('T')[0]
 
-spawnSync('git', ['-C', `${__dirname}/../kancolle-data`, 'add', '.'])
-spawnSync('git', ['-C', `${__dirname}/../kancolle-data`, 'commit', '-m', `"chore: autoupdate api_start2 for ${ts}"`])
-spawnSync('git', ['-C', `${__dirname}/../kancolle-data`, 'push'])
-
-console.log('pushed api_start2.json to kancolle-data')
+sh('git', '-C', `${__dirname}/../kancolle-data`, 'add', '.')
+sh('git', '-C', `${__dirname}/../kancolle-data`, 'commit', '--short', '-m', `"chore: autoupdate api_start2 for ${ts}"`)
+sh('git', '-C', `${__dirname}/../kancolle-data`, 'push', '-q')
