82ac571da27e6b95779836ca104cfe136fe44f1b
gk <gkpyon@gmail.com>
2019-02-22 18:58:47 +0900

    fix: update cookie

--- preload.js
+++ preload.js
@@ -22,16 +22,16 @@ document.addEventListener('DOMContentLoaded', () => {
   // UA
   webContent.session.setUserAgent(webContent.session.getUserAgent(), 'ja-JP')
   // Cookie
-  document.cookie = 'cklg=welcome;expires=Sun, 09 Feb 2019 09:00:09 GMT;domain=.dmm.com;path=/'
-  document.cookie = 'cklg=welcome;expires=Sun, 09 Feb 2019 09:00:09 GMT;domain=.dmm.com;path=/netgame/'
-  document.cookie = 'cklg=welcome;expires=Sun, 09 Feb 2019 09:00:09 GMT;domain=.dmm.com;path=/netgame_s/'
-  document.cookie = 'ckcy=1;expires=Sun, 09 Feb 2019 09:00:09 GMT;domain=osapi.dmm.com;path=/'
-  document.cookie = 'ckcy=1;expires=Sun, 09 Feb 2019 09:00:09 GMT;domain=203.104.209.7;path=/'
-  document.cookie = 'ckcy=1;expires=Sun, 09 Feb 2019 09:00:09 GMT;domain=www.dmm.com;path=/netgame/'
-  document.cookie = 'ckcy=1;expires=Sun, 09 Feb 2019 09:00:09 GMT;domain=log-netgame.dmm.com;path=/'
-  document.cookie = 'ckcy=1;expires=Sun, 09 Feb 2019 09:00:09 GMT;domain=.dmm.com;path=/'
-  document.cookie = 'ckcy=1;expires=Sun, 09 Feb 2019 09:00:09 GMT;domain=.dmm.com;path=/netgame/'
-  document.cookie = 'ckcy=1;expires=Sun, 09 Feb 2019 09:00:09 GMT;domain=.dmm.com;path=/netgame_s/'
+  document.cookie = 'cklg=welcome;expires=Thu, 16-Jan-2020 00:00:00 GMT;domain=.dmm.com;path=/'
+  document.cookie = 'cklg=welcome;expires=Thu, 16-Jan-2020 00:00:00 GMT;domain=.dmm.com;path=/netgame/'
+  document.cookie = 'cklg=welcome;expires=Thu, 16-Jan-2020 00:00:00 GMT;domain=.dmm.com;path=/netgame_s/'
+  document.cookie = 'ckcy=1;expires=Thu, 16-Jan-2020 00:00:00 GMT;domain=osapi.dmm.com;path=/'
+  document.cookie = 'ckcy=1;expires=Thu, 16-Jan-2020 00:00:00 GMT;domain=203.104.209.7;path=/'
+  document.cookie = 'ckcy=1;expires=Thu, 16-Jan-2020 00:00:00 GMT;domain=www.dmm.com;path=/netgame/'
+  document.cookie = 'ckcy=1;expires=Thu, 16-Jan-2020 00:00:00 GMT;domain=log-netgame.dmm.com;path=/'
+  document.cookie = 'ckcy=1;expires=Thu, 16-Jan-2020 00:00:00 GMT;domain=.dmm.com;path=/'
+  document.cookie = 'ckcy=1;expires=Thu, 16-Jan-2020 00:00:00 GMT;domain=.dmm.com;path=/netgame/'
+  document.cookie = 'ckcy=1;expires=Thu, 16-Jan-2020 00:00:00 GMT;domain=.dmm.com;path=/netgame_s/'
   // Catch API from game frame
   if (env.api_method === 'axios') {
     wait(
